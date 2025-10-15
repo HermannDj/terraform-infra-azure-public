f4e2e8bc9d4e66d1b1d4d5162320fb40d0949c30
HermannDj <harnaud.djoko@gmail.com>
2025-10-15 11:50:01 -0400

    fix error code on workflow and test

--- tests/test_terraform_infra.go
+++ tests/test_terraform_infra.go
@@ -8,12 +8,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
 func TestTerraformInfrastructure(t *testing.T) {
+	// Lire les variables d'environnement
 	resourceGroupName := os.Getenv("RESOURCE_GROUP_NAME")
-	acrName := os.Getenv("acr_name")
-	location := os.Getenv("location")
+	acrName := os.Getenv("ACR_NAME")
+	location := os.Getenv("LOCATION")
 
+	// Définir les options Terraform
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../terraform",
 		Vars: map[string]interface{}{
@@ -23,11 +24,13 @@ func TestTerraformInfrastructure(t *testing.T) {
 		},
 	}
 
+	// Détruire l'infrastructure à la fin du test
 	defer terraform.Destroy(t, terraformOptions)
-	t
-	erraform.InitAndApply(t, terraformOptions)s
 
-	outputACRName := terraform.Output(t, terraformOptions, "acr_name")
-	assert.Equal(t, acrName, outputACRName)
+	// Initialiser et appliquer Terraform
+	terraform.InitAndApply(t, terraformOptions)
 
+	// Récupérer la sortie Terraform
+	outputACRName := terraform.Output(t, terraformOptions, "acr_login_server") // correspond à ton output Terraform
+	assert.Equal(t, acrName+".azurecr.io", outputACRName)
 }
